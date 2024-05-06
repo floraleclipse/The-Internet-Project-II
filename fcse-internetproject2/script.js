b1242028686cf30485ca2380b620bd89f41c699c
FLORALECLXPSE\flora <em26356@westridge.org>
2024-05-06 14:54:04 -0700

    adding number generator

--- fcse-internetproject2/script.js
+++ fcse-internetproject2/script.js
@@ -5,16 +5,37 @@
  * https://api.thecatapi.com/v1/images/search
  */
 
-async function getCat() {
-  let response = await fetch("https://api.thecatapi.com/v1/images/search");
-  if (response.ok) {
-    let json = await response.json();
-    let catimg = json[0].url; 
-    document.getElementById("cat").src = catimg; 
-    console.log(json);
-  } else {
-    alert("HTTP-Error: " + response.status);
-  }
-}
+
+  let catCount = 0;
+        let randomNumber = Math.floor(Math.random() * 20) + 1;
+        console.log(randomNumber);
+
+        function checkGuess() {
+            let userGuess = parseInt(document.getElementById("guessInput").value);
+            if (userGuess === randomNumber) {
+                document.getElementById("result").innerText = "Congratulations! You guessed the correct number!";
+                if (++catCount === 3) {
+                    getCat();
+                    catCount = 0;
+                }
+            } else if (userGuess < randomNumber) {
+                document.getElementById("result").innerText = "Try again! The number is higher.";
+            } else {
+                document.getElementById("result").innerText = "Try again! The number is lower.";
+            }
+        }
+
+        async function getCat() {
+            let response = await fetch("https://api.thecatapi.com/v1/images/search");
+            if (response.ok) {
+                let json = await response.json();
+                let catImage = json[0].url;
+                document.getElementById("result").innerText = "Congratulations! You guessed the correct number! Here's a cat picture:";
+                document.getElementById("catImage").src = catImage;
+                document.getElementById("catImage").style.display = "block";
+            } else {
+                alert("Failed to fetch cat image. Please try again later.");
+            }
+        }
 
 getCat();
