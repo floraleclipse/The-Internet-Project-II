d91d1495fabc5a84b87790d854d3f5ffa52e8d0d
FLORALECLXPSE\flora <em26356@westridge.org>
2024-05-06 15:10:11 -0700

    adding fixed number generator

--- fcse-internetproject2/script.js
+++ fcse-internetproject2/script.js
@@ -4,9 +4,22 @@
  * Cat images API endpoint:
  * https://api.thecatapi.com/v1/images/search
  */
-let guessInput = document.getElementById("guessInput");
-let user_guesses = 0;
+/*let guessInput = document.getElementById("guessInput");
+let user_guesses = 0;*/
 
+
+
+async function getCat() {
+            let response = await fetch("https://api.thecatapi.com/v1/images/search");
+            if (response.ok) {
+                let json = await response.json();
+                let catImage = json[0].url;
+                document.getElementById("cat").src = catImage;
+                document.getElementById("cat").style.display = "block";
+            } else {
+                alert("Failed to fetch cat image. Please try again later.");
+            }
+        }
 
 
   let catCount = 0;
@@ -16,6 +29,7 @@ let user_guesses = 0;
         function checkGuess() {
             let userGuess = parseInt(document.getElementById("guessInput").value);
             if (userGuess === randomNumber) {
+                randomNumber = Math.floor(Math.random() * 20) + 1;
                 document.getElementById("result").innerText = "Congratulations! You guessed the correct number!";
                 if (++catCount === 3) {
                     getCat();
@@ -26,6 +40,7 @@ let user_guesses = 0;
             } else {
                 document.getElementById("result").innerText = "Try again! The number is lower.";
             }
+/* <<<<<<< HEAD
         }
 
         async function getCat() {
@@ -72,3 +87,6 @@ function check_guess (generated_number, user_guess){
         }
     }
 }
+=======
+        }
+>>>>>>> ca416b9 (adding fixed number generator) */
